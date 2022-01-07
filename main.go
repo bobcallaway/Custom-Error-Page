ba62382a5463caa0a879d54c21ce85ff6bd497f3
qizhicheng <qizhicheng@douban.com>
2022-01-07 10:42:00 +0800

    add templates

--- main.go
+++ main.go
@@ -6,8 +6,10 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/mcuadros/go-gin-prometheus"
 	"github.com/sirupsen/logrus"
+	"mime"
 	"os"
 	"strconv"
+	"strings"
 )
 
 const (
@@ -41,13 +43,21 @@ const (
 	// the location on disk of files served by the handler.
 	ErrFilesPathVar = "ERROR_FILES_PATH"
 
+	DefaultErrorFilesPath = "templates"
+
 	// DefaultFormatVar is the name of the environment variable indicating
 	// the default error MIME type that should be returned if either the
 	// client does not specify an Accept header, or the Accept header provided
 	// cannot be mapped to a file extension.
 	DefaultFormatVar = "DEFAULT_RESPONSE_FORMAT"
 
-	ServerName = "SERVER_NAME"
+	ServerNameVar = "SERVER_NAME"
+
+	DebugVar = "DEBUG"
+
+	ServerListenVar = "LISTEN"
+
+	DefaultServerListen = ":8080"
 )
 
 func setupRouter() *gin.Engine {
@@ -81,7 +91,22 @@ func setupRouter() *gin.Engine {
 	p := ginprometheus.NewPrometheus("gin")
 
 	p.Use(r)
-	r.GET("/", errorHandler)
+	templateRoot := os.Getenv(ErrFilesPathVar)
+	if templateRoot == "" {
+		templateRoot = DefaultErrorFilesPath
+	}
+	defaultFormat := os.Getenv(DefaultFormatVar)
+	if defaultFormat == "" {
+		defaultFormat = "text/html"
+	}
+	isDebug := false
+	if os.Getenv(DebugVar) != "" {
+		isDebug = true
+	}
+
+	serverName := os.Getenv(ServerNameVar)
+	r.LoadHTMLGlob(fmt.Sprintf("%s/*", templateRoot))
+	r.GET("/", errorHandler(defaultFormat, serverName, isDebug))
 	r.GET("/healthz", func(c *gin.Context) {
 		c.String(200, "OK")
 	})
@@ -90,26 +115,73 @@ func setupRouter() *gin.Engine {
 
 func main() {
 	r := setupRouter()
-
-	r.Run(":29090")
+	listen := os.Getenv(ServerListenVar)
+	if listen == "" {
+		listen = DefaultServerListen
+	}
+	_ = r.Run(listen)
 }
-func errorHandler(c *gin.Context) {
-	if os.Getenv("DEBUG") != "" {
-		c.Writer.Header().Set(FormatHeader, c.Request.Header.Get(FormatHeader))
-		c.Writer.Header().Set(CodeHeader, c.Request.Header.Get(CodeHeader))
-		c.Writer.Header().Set(ContentType, c.Request.Header.Get(ContentType))
-		c.Writer.Header().Set(OriginalURI, c.Request.Header.Get(OriginalURI))
-		c.Writer.Header().Set(Namespace, c.Request.Header.Get(Namespace))
-		c.Writer.Header().Set(IngressName, c.Request.Header.Get(IngressName))
-		c.Writer.Header().Set(ServiceName, c.Request.Header.Get(ServiceName))
-		c.Writer.Header().Set(ServicePort, c.Request.Header.Get(ServicePort))
-		c.Writer.Header().Set(RequestId, c.Request.Header.Get(RequestId))
+func errorHandler(defaultFormat string, serverName string, debug bool) func(*gin.Context) {
+	defaultExts, err := mime.ExtensionsByType(defaultFormat)
+	if err != nil || len(defaultExts) == 0 {
+		panic("couldn't get file extension for default format")
 	}
-	errCode := c.Request.Header.Get(CodeHeader)
-	code, err := strconv.Atoi(errCode)
-	if err != nil {
-		code = 404
-		logrus.Printf("unexpected error reading return code: %v. Using %v", err, code)
+	defaultExt := defaultExts[0]
+	return func(c *gin.Context) {
+		ext := defaultExt
+		if debug {
+			c.Writer.Header().Set(FormatHeader, c.Request.Header.Get(FormatHeader))
+			c.Writer.Header().Set(CodeHeader, c.Request.Header.Get(CodeHeader))
+			c.Writer.Header().Set(ContentType, c.Request.Header.Get(ContentType))
+			c.Writer.Header().Set(OriginalURI, c.Request.Header.Get(OriginalURI))
+			c.Writer.Header().Set(Namespace, c.Request.Header.Get(Namespace))
+			c.Writer.Header().Set(IngressName, c.Request.Header.Get(IngressName))
+			c.Writer.Header().Set(ServiceName, c.Request.Header.Get(ServiceName))
+			c.Writer.Header().Set(ServicePort, c.Request.Header.Get(ServicePort))
+			c.Writer.Header().Set(RequestId, c.Request.Header.Get(RequestId))
+		}
+		errCode := c.Request.Header.Get(CodeHeader)
+		code, err := strconv.Atoi(errCode)
+		if err != nil {
+			code = 404
+			logrus.Printf("unexpected error reading return code: %v. Using %v", err, code)
+		}
+		format := c.Request.Header.Get(FormatHeader)
+		if format == "" {
+			format = defaultFormat
+			logrus.Printf("format not specified. Using %v", format)
+		}
+
+		cext, err := mime.ExtensionsByType(format)
+		if err != nil {
+			logrus.Printf("unexpected error reading media type extension: %v. Using %v", err, ext)
+			format = defaultFormat
+		} else if len(cext) == 0 {
+			logrus.Printf("couldn't get media type extension. Using %v", ext)
+		} else {
+			ext = cext[0]
+		}
+		c.Writer.Header().Set(ContentType, format)
+
+		if !strings.HasPrefix(ext, ".") {
+			ext = "." + ext
+		}
+		// special case for compatibility
+		if ext == ".htm" {
+			ext = ".html"
+		}
+		openVariables := gin.H{
+			"code":        code,
+			"originalUrl": c.Request.Header.Get(OriginalURI),
+			"ingressName": c.Request.Header.Get(IngressName),
+			"serverName":  serverName,
+			"requestId":   c.Request.Header.Get(RequestId),
+		}
+		if ext == ".html" {
+			c.HTML(code, "default.html", openVariables)
+		} else {
+			c.JSON(code, openVariables)
+		}
 	}
-	c.String(code, fmt.Sprintf("server name: '%s'", os.Getenv(ServerName)))
+
 }
--- main_test.go
+++ main_test.go
@@ -26,12 +26,38 @@ func TestCodeHeader(t *testing.T) {
 }
 
 func TestServerName(t *testing.T) {
-	_ = os.Setenv("SERVER_NAME", "test-server")
+	_ = os.Setenv(ServerNameVar, "test-server")
 	gin := setupRouter()
 
 	w := httptest.NewRecorder()
-
 	req, _ := http.NewRequest("GET", "/", nil)
+	req.Header.Set("X-Ingress-Name", "test-ingress")
 	gin.ServeHTTP(w, req)
 	assert.Contains(t, w.Body.String(), "test-server")
+
+	w = httptest.NewRecorder()
+	_ = os.Setenv(ServerNameVar, "")
+	gin = setupRouter()
+	req, _ = http.NewRequest("GET", "/", nil)
+	req.Header.Set("X-Ingress-Name", "test-ingress")
+	gin.ServeHTTP(w, req)
+	assert.Contains(t, w.Body.String(), "test-ingress")
+}
+
+func TestContentType(t *testing.T) {
+	gin := setupRouter()
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/", nil)
+	req.Header.Set("X-Format", "text/html")
+	gin.ServeHTTP(w, req)
+	assert.Contains(t, w.Body.String(), "html")
+	assert.Equal(t, w.Header().Get(ContentType), "text/html")
+
+	w = httptest.NewRecorder()
+	gin = setupRouter()
+	req, _ = http.NewRequest("GET", "/", nil)
+	req.Header.Set("X-Format", "application/json")
+	gin.ServeHTTP(w, req)
+	assert.Contains(t, w.Body.String(), "{")
+	assert.Equal(t, w.Header().Get(ContentType), "application/json")
 }
